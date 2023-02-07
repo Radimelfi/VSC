d854cc0f33fcf862ce6dc1050681bc0978b0615f
Radimelfi <radimelfibautistaantigua@gmail.com>
2023-02-07 12:47:21 -0400

    README.md

--- Day17.py/Quiz_game/main.py
+++ Day17.py/Quiz_game/main.py
@@ -1,4 +1,21 @@
 
 from data import question_data 
-from question_model 
+from question_model import Question 
+from quiz_brain import QuizBrain
+
+question_quiz = []
+for question in question_data:
+    question_text = question ["text"]
+    question_answer = question ["answer"]
+    new_question = Question(question_text, question_answer)
+    question_quiz.append(new_question)
+    print(new_question)
+    
+
+quiz = QuizBrain(question_quiz)
+
+while quiz.still_has_question():
+    quiz.next_question()
+print (f"you've completed the quiz")
+print (f"your final score was: {quiz.score}/{len(question_data)}")
 
